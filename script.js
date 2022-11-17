a6180e6a9235ad593f2dc53b348ebdbf5ce0d7f7
mmarhalim <115380281+mmarhalim@users.noreply.github.com>
2022-11-17 09:15:59 -0700

    learning react

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-const element = React.createElement(
-  'h1', // type
-  {
-    className: 'title',
-    onClick: () => { console.log('clicked'); },
-    style: { fontFamily: 'sans-serif', color: 'rgb(255, 100, 100)' }
-  }, // props
-  'Hello World!' // children
-);
+const formulateGreeting = (user) => {
+  if (user) {
+    let greeting;
+    switch (user.language) {
+      case 'es':
+        greeting = 'Hola';
+        break;
+      case 'de':
+        greeting = 'Hallo';
+        break;
+      default:
+        greeting = 'Hello';
+    }
+    return (
+      <h1 className={`greeting ${user.language}`}>
+        {`${greeting} ${user.firstName} ${user.lastName}`}
+      </h1>
+    );
+  }
+
+  return <h1 className="greeting">Hello my friend</h1>;
+};
+
+const user = {
+  language: 'es',
+  firstName: 'Javier',
+  lastName: 'Bandela'
+};
+
+const element = formulateGreeting(user);
 
 ReactDOM.render(
   element,
   document.getElementById('root')
 );
-
-ReactDOM.render(
-  element,
-  document.getElementById('root2')
-);
-
-document.querySelectorAll('.button')
-  .forEach(domContainer => {
-    const messageId = parseInt(domContainer.dataset.messageId);
-
-    ReactDOM.render(
-      React.createElement(
-        'button',
-        {
-          onClick: () => { console.log(`liked message: ${messageId}`); }
-        },
-        'Like'
-      ),
-      domContainer
-    )
-  });
